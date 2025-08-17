29b153e873d0de74d8a8662d15dbab67ba0772fb
NicolasValentimX <nvalentim824@gmail.com>
2025-08-17 00:38:00 -0300

    Primeiro commit aula C#

---
+++ Array_Multidimensional/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Array multidimensional");
+
+
+int[,] a;
+
+a = new int[2, 2];
+a[0, 0] = 0;
+a[0, 1] = 1;
+a[1, 0] = 2;
+a[1, 1] = 3;
+
+Console.WriteLine(a[0, 0]);
+Console.WriteLine(a[0, 1]);
+Console.WriteLine(a[1, 0]);
+Console.WriteLine(a[1, 1]);
+
+
+
+Console.ReadKey();
